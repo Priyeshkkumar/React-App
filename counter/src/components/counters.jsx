1f722b93b8f255e836ddb513bd8d030ab434fa46
Priyeshkkumar <priyeshkkumar@gmail.com>
2020-12-15 00:19:35 +0530

    Destructuring Arguments

--- counter/src/components/counters.jsx
+++ counter/src/components/counters.jsx
@@ -2,17 +2,20 @@ import React, { Component } from "react";
 import Counter from "./counter";
 class Counters extends Component {
   render() {
+    // Pick properties of props object we are interested in
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+
     // Use map method to render the array of Counters
     return (
       <div>
         <button
           // We are now raising this event and bubbling this to parent
-          onClick={this.props.onReset}
+          onClick={onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.props.counters.map((counter) => (
+        {counters.map((counter) => (
           // key is an attribute for uniquely identifying element while value and selected
           // attributes are properties of each counter object
           <Counter
@@ -22,8 +25,8 @@ class Counters extends Component {
             // Passing the whole counter object
             counter={counter}
             // passing "onDelete" as a prop to "Component"
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
           ></Counter>
         ))}
       </div>
